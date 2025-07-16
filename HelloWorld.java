3c778358ea4e9b7587f684ded75342ca7f99bbd4
Angus <angus.wong@hk.elab.elab.aswatson.com>
2025-07-16 15:05:33 +0800

    cscsssc

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!hkjasdppph");
+        System.out.println("Hello, World!hkjasdasdppph");
     }
 } 
