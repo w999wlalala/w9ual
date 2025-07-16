45ec9bef4bb52bd9e35b78123e4ad5a45473d948
Angus <angus.wong@hk.elab.elab.aswatson.com>
2025-07-16 15:05:10 +0800

    cscsssc

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ from datetime import datetime
 port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = "wa9999253@gmail.com"
-PASSWORD = "qbocnpmlhcbdyhrv"  # Remove spaces from app password
+PASSWORD = "zktw jybz lzyr dkyp"  # Remove spaces from app password
 TARGET_EMAIL = "wyhong0826@gmail.com"
 
 # Add detailed logging function
